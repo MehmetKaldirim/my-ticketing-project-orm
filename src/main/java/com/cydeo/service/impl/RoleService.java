73d1f366ebd45183149b1b005582ba3cf9784214
Mehmet <mathdenizi@gmail.com>
2022-05-06 11:32:22 +0200

    Begin the creating of Businnes Logic

---
+++ src/main/java/com/cydeo/service/impl/RoleService.java
@@ -0,0 +1,18 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.RoleDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class RoleService implements com.cydeo.service.RoleService {
+    @Override
+    public List<RoleDTO> listAllRoles() {
+        return null;
+    }
+
+    @Override
+    public RoleDTO findById(Long Id) {
+        return null;
+    }
+}
